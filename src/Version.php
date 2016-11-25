0a2d50843fd32f4234469d6799c3f2bd8ee538a0
Arne Blankerts <Arne@Blankerts.de>
2016-11-25 14:45:54 +0100

    Made version value object SemVer aware

--- src/Version.php
+++ src/Version.php
@@ -18,6 +18,31 @@ final class Version
     private $version;
 
     /**
+     * @var int
+     */
+    private $major;
+
+    /**
+     * @var int
+     */
+    private $minor;
+
+    /**
+     * @var int
+     */
+    private $patch;
+
+    /**
+     * @var string
+     */
+    private $releaseType = 'stable';
+
+    /**
+     * @var int
+     */
+    private $releaseTypeCount = 0;
+
+    /**
      * @param string $version
      *
      * @throws InvalidVersionException
@@ -38,9 +63,135 @@ final class Version
     }
 
     /**
+     * @return int
+     */
+    public function getMajorVersion() {
+        return $this->major;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMinorVersion() {
+        return $this->minor;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPatchLevel() {
+        return $this->patch;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReleaseType() {
+        return $this->releaseType;
+    }
+
+    /**
+     * @return int
+     */
+    public function getReleaseTypeCount() {
+        return $this->releaseTypeCount;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isStableVersion() {
+        return $this->releaseType === 'stable';
+    }
+
+    /**
+     * @return bool
+     */
+    public function isBetaVersion() {
+        return $this->releaseType === 'beta';
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAlphaVersion() {
+        return $this->releaseType === 'alpha';
+    }
+
+    /**
+     * @return bool
+     */
+    public function isPatchVersion() {
+        return $this->releaseType === 'patch';
+    }
+
+    /**
+     * @return bool
+     */
+    public function isDevelopmentVersion() {
+        return $this->releaseType === 'dev';
+    }
+
+    /**
      * @param string $version
+     *
+     * @throws InvalidVersionException
      */
     private function ensureVersionIsValid($version)
     {
+        $regex = "/^
+            (?<Major>(0|(?:[1-9][0-9]*)))
+            \\.
+            (?<Minor>(0|(?:[1-9][0-9]*)))
+            \\.
+            (?<Patch>(0|(?:[1-9][0-9]*)))
+            (?:
+                -
+                (?<ReleaseType>(?:(dev|beta|b|RC|alpha|a|patch|p)))
+                (?:
+                    (?<ReleaseTypeCount>[0-9])
+                )?
+            )?       
+        \$/x";
+
+        if (preg_match($regex, $version, $matches) !== 1) {
+            throw new InvalidVersionException(
+                sprintf("Version string '%s' does not follow SemVer semantics", $version)
+            );
+        }
+
+        $this->initValues($matches);
+    }
+
+    /**
+     * @param string[] $matches
+     */
+    private function initValues(array $matches)
+    {
+        $this->major = $matches['Major'];
+        $this->minor = $matches['Minor'];
+        $this->patch = $matches['Patch'];
+
+        if (!isset($matches['ReleaseType'])) {
+            return;
+        }
+
+        if (strlen($matches['ReleaseType']) === 1) {
+            $expanded = [
+                'a' => 'alpha',
+                'b' => 'beta',
+                'p' => 'patch'
+            ];
+            $matches['ReleaseType'] = $expanded[$matches['ReleaseType']];
+        }
+
+        $this->releaseType = $matches['ReleaseType'];
+
+        if (!isset($matches['ReleaseTypeCount'])) {
+            $this->releaseTypeCount = 1;
+            return;
+        }
+
+        $this->releaseTypeCount = $matches['ReleaseTypeCount'];
     }
 }
--- tests/VersionTest.php
+++ tests/VersionTest.php
@@ -13,13 +13,58 @@ namespace PharIo\Manifest;
 use PHPUnit\Framework\TestCase;
 
 /**
- * @covers PharIo\Manifest\Version
+ * @covers \PharIo\Manifest\Version
  */
 class VersionTest extends TestCase
 {
-    public function testCanBeCreatedForValidVersion()
+
+    /**
+     * @dataProvider validVersionStrings
+     */
+    public function testCanBeCreatedForValidVersion($versionString)
     {
-        $this->assertInstanceOf(Version::class, new Version('5.6.5'));
+        $this->assertInstanceOf(Version::class, new Version($versionString));
+    }
+
+    public function validVersionStrings() {
+        return [
+            '1.0.0' => ['1.0.0'],
+            '15.0.23' => ['15.0.23'],
+            '1.0.0-dev' => ['1.0.0-dev'],
+            '1.0.0-alpha1' => ['1.0.0-alpha1'],
+            '1.0.0-beta2' => ['1.0.0-beta2'],
+            '1.0.0-a3' => ['1.0.0-a3'],
+            '1.0.0-b4' => ['1.0.0-b4'],
+            '1.0.0-RC5' => ['1.0.0-RC5'],
+            '2.0.4-p6' => ['2.0.4-p6'],
+            '2.0.4-patch7' => ['2.0.4-patch7']
+        ];
+    }
+
+    /**
+     * @covers \PharIo\Manifest\InvalidVersionException
+     * @dataProvider invalidVersionStrings
+     */
+    public function testCannotBeCreatedForInvalidVersion($versionString)
+    {
+        $this->expectException(InvalidVersionException::class);
+        new Version($versionString);
+    }
+
+    public function invalidVersionStrings() {
+        return [
+            'a.b.c' => ['a.b.c'],
+            'not-a-version' => ['not-a-version'],
+            '1.0' => ['1.0'],
+            '1.2.3-foo' => ['1.2.3-foo'],
+            '1.2.3-foo2' => ['1.2.3-foo2'],
+            '1.2.3.4' => ['1.2.3.4'],
+            '1.2.3_foo' => ['1.2.3_foo'],
+            '1.0-beta1' => ['1.0-beta1'],
+            '01.2.3' => ['01.2.3'],
+            '0.00.1' => ['0.00.1'],
+            '0-a1' => ['0-a1']
+        ];
     }
 
     public function testCanBeUsedAsString()
@@ -27,13 +72,68 @@ class VersionTest extends TestCase
         $this->assertEquals('5.6.5', new Version('5.6.5'));
     }
 
-    /**
-     * @covers PharIo\Manifest\InvalidVersionException
-     */
-    public function testCannotBeCreatedForInvalidVersion()
-    {
-        $this->expectException(InvalidVersionException::class);
-
-        new Version('invalid');
+    public function testMajorVersionCanBeRetrieved() {
+        $this->assertEquals(1, (new Version('1.0.0'))->getMajorVersion());
     }
+
+    public function testMinorVersionCanBeRetrieved() {
+        $this->assertEquals(1, (new Version('0.1.0'))->getMinorVersion());
+    }
+
+    public function testPatchLevelCanBeRetrieved() {
+        $this->assertEquals(1, (new Version('0.0.1'))->getPatchLevel());
+    }
+
+    public function testIsStableVersionReturnsTrueOnStableRelease() {
+        $this->assertTrue((new Version('1.0.0'))->isStableVersion());
+    }
+
+    public function testIsStableVersionReturnsFalseOnNonStableRelease() {
+        $this->assertFalse((new Version('1.0.0-dev'))->isStableVersion());
+    }
+
+    public function testIsDevelopmentVersionReturnsTrueOnDevelopmentRelease() {
+        $this->assertTrue((new Version('1.0.0-dev'))->isDevelopmentVersion());
+    }
+
+    public function testIsDevelopmentVersionReturnsFalseOnNonDevelopmentRelease() {
+        $this->assertFalse((new Version('1.0.0'))->isDevelopmentVersion());
+    }
+
+    public function testIsPatchVersionReturnsTrueOnPatchRelease() {
+        $this->assertTrue((new Version('1.0.0-p1'))->isPatchVersion());
+    }
+
+    public function testIsPatchVersionReturnsFalseOnNonPatchRelease() {
+        $this->assertFalse((new Version('1.0.0'))->isPatchVersion());
+    }
+
+    public function testIsAlphaVersionReturnsTrueOnAlphaRelease() {
+        $this->assertTrue((new Version('1.0.0-a1'))->isAlphaVersion());
+    }
+
+    public function testIsAlphaVersionReturnsFalseOnNonAlphaRelease() {
+        $this->assertFalse((new Version('1.0.0'))->isAlphaVersion());
+    }
+
+    public function testIsBetaVersionReturnsTrueOnBetaRelease() {
+        $this->assertTrue((new Version('1.0.0-b1'))->isBetaVersion());
+    }
+
+    public function testIsBetaVersionReturnsFalseOnNonBetaRelease() {
+        $this->assertFalse((new Version('1.0.0'))->isBetaVersion());
+    }
+
+    public function testCorrectReleaseTypeIsReturnedForBetaVersion() {
+        $this->assertEquals('beta', (new Version('1.0.0-b1'))->getReleaseType());
+    }
+
+    public function testCorrectReleaseTypeCountIsReturnedForBetaVersion() {
+        $this->assertEquals(1, (new Version('1.0.0-beta'))->getReleaseTypeCount());
+    }
+
+    public function testCorrectReleaseTypeCountIsReturnedForSecondBetaVersion() {
+        $this->assertEquals(2, (new Version('1.0.0-b2'))->getReleaseTypeCount());
+    }
+
 }
