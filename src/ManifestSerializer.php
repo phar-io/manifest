573de4e1ad59f7a076682fdc00f43ec8d578177e
Arne Blankerts <Arne@Blankerts.de>
2016-11-26 23:35:32 +0100

    Add Manifest Serializer

---
+++ src/ManifestSerializer.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace PharIo\Manifest;
+
+use XMLWriter;
+
+class ManifestSerializer {
+
+    /**
+     * @var XMLWriter
+     */
+    private $xmlWriter;
+
+    public function serializeToFile(Manifest $manifest, $filename) {
+        file_put_contents(
+            $filename,
+            $this->serializeToString($manifest)
+        );
+    }
+
+    public function serializeToString(Manifest $manifest) {
+        $this->startDocument();
+
+        $this->addContains($manifest->getName(), $manifest->getVersion(), $manifest->getType());
+        $this->addCopyright($manifest->getCopyrightInformation());
+        $this->addRequirements($manifest->getRequirements());
+        $this->addBundles($manifest->getBundledComponents());
+
+        return $this->finishDocument();
+    }
+
+    private function startDocument() {
+        $xmlWriter = new XMLWriter();
+        $xmlWriter->openMemory();
+        $xmlWriter->setIndent(true);
+        $xmlWriter->setIndentString(str_repeat(' ', 4));
+        $xmlWriter->startDocument('1.0', 'UTF-8');
+        $xmlWriter->startElement('phar');
+        $xmlWriter->writeAttribute('xmlns', 'https://phar.io/xml/manifest/1.0');
+
+        $this->xmlWriter = $xmlWriter;
+    }
+
+    private function finishDocument() {
+        $this->xmlWriter->endElement();
+        $this->xmlWriter->endDocument();
+        return $this->xmlWriter->outputMemory();
+    }
+
+    private function addContains($name, Version $version, Type $type) {
+        $this->xmlWriter->startElement('contains');
+        $this->xmlWriter->writeAttribute('name', $name);
+        $this->xmlWriter->writeAttribute('version', (string)$version);
+
+        switch (true) {
+            case $type->isApplication(): {
+                $this->xmlWriter->writeAttribute('type', 'application');
+                break;
+            }
+
+            case $type->isLibrary(): {
+                $this->xmlWriter->writeAttribute('type', 'library');
+                break;
+            }
+
+            case $type->isExtension(): {
+                /** @var $type Extension */
+                $this->xmlWriter->writeAttribute('type', 'extension');
+                $this->addExtension($type->getApplication(), $type->getVersionConstraint());
+                break;
+            }
+
+            default: {
+                $this->xmlWriter->writeAttribute('type', 'custom');
+            }
+        }
+        $this->xmlWriter->endElement();
+    }
+
+    private function addCopyright(CopyrightInformation $copyrightInformation) {
+        $this->xmlWriter->startElement('copyright');
+
+        foreach($copyrightInformation->getAuthors() as $author) {
+            $this->xmlWriter->startElement('author');
+            $this->xmlWriter->writeAttribute('name', $author->getName() );
+            $this->xmlWriter->writeAttribute('email', (string)$author->getEmail());
+            $this->xmlWriter->endElement();
+        }
+
+        $license = $copyrightInformation->getLicense();
+        $this->xmlWriter->startElement('license');
+        $this->xmlWriter->writeAttribute('type', $license->getName() );
+        $this->xmlWriter->writeAttribute('url', $license->getUrl() );
+        $this->xmlWriter->endElement();
+
+        $this->xmlWriter->endElement();
+    }
+
+    private function addRequirements(RequirementCollection $requirementCollection) {
+        $phpRequirement = new VersionConstraint('*');
+        $extensions=[];
+        foreach($requirementCollection as $requirement) {
+            if ($requirement instanceof PhpVersionRequirement) {
+                $phpRequirement = $requirement->getVersionConstraint();
+                continue;
+            }
+            if ($requirement instanceof PhpExtensionRequirement) {
+                $extensions[] = (string)$requirement;
+            }
+        }
+
+        $this->xmlWriter->startElement('requires');
+        $this->xmlWriter->startElement('php');
+        $this->xmlWriter->writeAttribute('version', (string)$phpRequirement);
+
+        foreach($extensions as $extension) {
+            $this->xmlWriter->startElement('ext');
+            $this->xmlWriter->writeAttribute('name', $extension);
+            $this->xmlWriter->endElement();
+        }
+
+        $this->xmlWriter->endElement();
+        $this->xmlWriter->endElement();
+    }
+
+    private function addBundles(BundledComponentCollection $bundledComponentCollection) {
+        $this->xmlWriter->startElement('bundles');
+
+        foreach($bundledComponentCollection as $bundledComponent) {
+            $this->xmlWriter->startElement('component');
+            $this->xmlWriter->writeAttribute('name', $bundledComponent->getName());
+            $this->xmlWriter->writeAttribute('name', (string)$bundledComponent->getVersion());
+            $this->xmlWriter->endElement();
+        }
+
+        $this->xmlWriter->endElement();
+    }
+
+    private function addExtension($application, VersionConstraint $versionConstraint) {
+        $this->xmlWriter->startElement('extension');
+        $this->xmlWriter->writeAttribute('for', $application);
+        $this->xmlWriter->writeAttribute('compatible', (string)$versionConstraint);
+        $this->xmlWriter->endElement();
+    }
+
+}
